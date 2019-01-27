d961721e5e3c8fe5b438398186ecf5c0a91bf13b
Liam H <35095843+ir-3@users.noreply.github.com>
2019-01-26 22:21:53 -0600

    Remove the _ from the repl variables

--- jishaku/repl/__init__.py
+++ jishaku/repl/__init__.py
@@ -25,11 +25,11 @@ def get_var_dict_from_ctx(ctx: commands.Context):
     """
 
     return {
-        '_author': ctx.author,
-        '_bot': ctx.bot,
-        '_channel': ctx.channel,
-        '_ctx': ctx,
-        '_guild': ctx.guild,
-        '_message': ctx.message,
-        '_msg': ctx.message
+        'author': ctx.author,
+        'bot': ctx.bot,
+        'channel': ctx.channel,
+        'ctx': ctx,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        'msg': ctx.message
     }
